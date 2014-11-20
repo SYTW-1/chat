027bdd81000797d09797a288149c821afd589441
alu0100611298 <alu0100611298@ull.edu.es>
2014-11-20 13:54:13 +0000

    merge conflict

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -8,55 +8,6 @@ require 'rubygems'
 require 'rspec'
 
 include Rack::Test::Methods
-=begin
-describe "Test APP chat: Comprobacion de enlaces" do
-
-	def app
-	   Sinatra::Application
-	end
-
-   before :each do
-	  @browser = Selenium::WebDriver.for :firefox
-	  @site = 'https://chat-blazee.herokuapp.com/'
-	  if (ARGV[0].to_s == "local")
-		 @site = 'localhost:9292/'
-	  end
-	  @browser.get(@site)
-   end
-   
-   after :each do
-	  @browser.quit
-   end
-
-   it "The user Prueba can access to chat" do
-	 	@browser.manage.timeouts.implicit_wait = 5
-		element = @browser.find_element(:id,"username")
-		element.send_keys("Prueba")
-		element.submit 
-	 	expect(@browser.find_element(:id,"welcome").text).to eq("Welcome: Prueba\nLog Out")
-   end
-   it "The user send a post" do
-   		@browser.get(@site)
-	 	@browser.manage.timeouts.implicit_wait = 5
-		element = @browser.find_element(:id,"username")
-		element.send_keys("Prueba")
-		element.submit 
-	 	element = @browser.find_element(:id,"text")
-	 	element.send_keys("Hola")
-		element.send_keys:return
-		expect(@browser.find_element(:id,"name").text).to eq("Hola")
-   end
-   it "User prueba logout" do
-	 	@browser.manage.timeouts.implicit_wait = 5
-		element = @browser.find_element(:id,"username")
-		element.send_keys("Prueba")
-		element.submit 
-	 	element = @browser.find_element(:id,"logout")
-		element.click
-		expect(@browser.find_element(:id,"title").text).to eq("Please sign in")
-   end
-end
-=end
 describe "Test APP chat: Comprobacion de funciones" do
 
 	def app
@@ -84,7 +35,7 @@ describe "Test APP chat: Comprobacion de funciones" do
 		expect(last_response.body).to eq("")
 	end
 
-	it "Envio" do
+	it "Envio con sesion" do
 		get '/send'
 		expect(last_response.body).to eq("")
 	end
